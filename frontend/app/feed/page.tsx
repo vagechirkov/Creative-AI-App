c5fd7ac26727a134043d35643d51ccb45d25e05b
vagechirkov <vagechirkov@gmail.com>
2023-01-03 20:25:17 +0100

    add unique user id

--- frontend/app/feed/page.tsx
+++ frontend/app/feed/page.tsx
@@ -1,10 +1,13 @@
+const {v4: uuidv4} = require('uuid');
+
 import FeedImage from "../../components/FeedImage";
 
-export default function Feed() {
 
+export default function Feed() {
+    const userId = uuidv4();
     return (
         <main>
-            <FeedImage wsUrl={"ws://localhost:8000/ws/client3"}/>
+            <FeedImage wsUrl={`ws://localhost:8000/ws/${userId}`}/>
         </main>
     )
 }
