c09825e6d5ddcfe9090981b75e4000a738ab2deb
vagechirkov <vagechirkov@gmail.com>
2023-01-05 11:16:13 +0100

    clean frontend repo

--- frontend/app/feed/page.tsx
+++ frontend/app/feed/page.tsx
@@ -4,7 +4,7 @@ import FeedImages from "../../components/FeedImages";
 
 
 export default async function Feed() {
-    // Read environment variable from .env file
+    // Read environment variable from .env file or build.env file when deployed
     const WSBasePath = process.env.NEXT_PUBLIC_BACKEND_API;
     const userId: string = nanoid();
     return (
