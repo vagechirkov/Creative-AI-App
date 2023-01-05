380566c1ea7c1a5f04cd462255e40816da0a992b
vagechirkov <vagechirkov@gmail.com>
2023-01-05 11:53:06 +0100

    refactor backend to have multiple feeds

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,14 +1,11 @@
-from typing import List
-
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-
-from app.models.feed import FeedImage, ImageReaction
-
 from fastapi_restful.tasks import repeat_every
+
+from app.models.feed import ImageReaction
+from app.utils.websocket_utils import ConnectionManager, create_feed_image
 
 app = FastAPI()
 
-last_three_images: List[FeedImage] = []
 
 urls = [
     'https://lexica-serve-encoded-images2.sharif.workers.dev/md/3761c6dd-a10f-4a76-bec5-27d508698840',
@@ -22,83 +19,49 @@ urls = [
     'https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/0fe9b428-033b-44c3-a97c-dec8e3d42a8c'
 ]
 
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-        # send the current feed image to the new connection
-        if last_three_images:
-            await websocket.send_json(last_three_images[-1].dict())
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def broadcast(self, message: FeedImage):
-        for connection in self.active_connections:
-            await connection.send_json(message.dict())
-
-
-manager = ConnectionManager()
+feeds = []
 
 
 @app.on_event("startup")
+def startup_event():
+    # create the all feeds and set up the connection manager for each feed
+    for i in range(1, 4):
+        feed = ConnectionManager(create_feed_image(i, urls[i - 1]), feed_id=i)
+        feeds.append(feed)
+
+
 @repeat_every(seconds=15)
-async def broadcast_feed():
-    # move the last url to the first position
-    urls.insert(0, urls.pop())
-
-    # create a new feed image
-    feed_image = FeedImage(
-        id=last_three_images[-1].id + 1 if last_three_images else 1,
-        url=urls[0],
-        reactions=[
-            ImageReaction(emoji='😍', count=0),
-            ImageReaction(emoji='👍', count=0),
-            ImageReaction(emoji='😐', count=0),
-            ImageReaction(emoji='👎', count=0),
-            ImageReaction(emoji='🤮', count=0),
-        ],
-        active_users=len(manager.active_connections)
-    )
-    # remove the last image
-    if last_three_images:
-        last_three_images.pop(0)
-
-    # add the new image
-    last_three_images.append(feed_image)
-
-    await manager.broadcast(feed_image)
+async def broadcast_all_feeds():
+    # broadcast the new feed image to all feeds
+    for feed in feeds:
+        image = create_feed_image(feed.current_feed_image.id + 1, urls[feed.current_feed_image.id])
+        await feed.broadcast(image)
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await manager.connect(websocket)
+@app.websocket("/ws/{client_id}/{feed_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str, feed_id: int):
+    if feed_id in range(feeds.__len__()):
+        feed = feeds[feed_id]
+        await feed.connect(websocket)
 
-    try:
-        while True:
-            # receive image reaction
-            reaction_data = await websocket.receive_json()
-            reaction_data = ImageReaction(**reaction_data)
+        try:
+            while True:
+                # receive image reaction
+                reaction_data = await websocket.receive_json()
+                reaction_data = ImageReaction(**reaction_data)
 
-            # update feed image reactions
-            image = last_three_images[-1]
-            if reaction_data.emoji in [reaction.emoji for reaction in image.reactions]:
-                for reaction in image.reactions:
-                    if reaction.emoji == reaction_data.emoji:
-                        reaction.count += 1
-            else:
-                image.reactions.append(reaction_data)
+                # update feed image reactions
+                image = feed.current_feed_image
+                if reaction_data.emoji in [reaction.emoji for reaction in image.reactions]:
+                    for reaction in image.reactions:
+                        if reaction.emoji == reaction_data.emoji:
+                            reaction.count += 1
+                else:
+                    image.reactions.append(reaction_data)
 
-            await manager.broadcast(image)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
+                await feed.broadcast(image)
+        except WebSocketDisconnect:
+            feed.disconnect(websocket)
 
 
-@app.get("/home")
-async def home():
-    return {'message': 'Hello World'}
+
---
+++ backend/app/app/utils/websocket_utils.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from fastapi import WebSocket
+
+from app.models.feed import FeedImage, ImageReaction
+
+
+class ConnectionManager:
+    def __init__(self, image: FeedImage, feed_id: int):
+        self.feed_id = feed_id
+        self.active_connections: List[WebSocket] = []
+        self.current_feed_image: FeedImage = image
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+        # send the current feed image to the new connection
+        if self.current_feed_image:
+            await websocket.send_json(self.current_feed_image.dict())
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: FeedImage):
+        self.current_feed_image = message
+        for connection in self.active_connections:
+            await connection.send_json(message.dict())
+
+
+async def broadcast_new_feed_image(manager: ConnectionManager, image: FeedImage):
+    await manager.broadcast(image)
+
+def create_feed_image(image_id: int, url: str) -> FeedImage:
+    return FeedImage(
+        id=image_id,
+        url=url,
+        reactions=[
+            ImageReaction(emoji='😍', count=0),
+            ImageReaction(emoji='👍', count=0),
+            ImageReaction(emoji='😐', count=0),
+            ImageReaction(emoji='👎', count=0),
+            ImageReaction(emoji='🤮', count=0),
+        ],
+        active_users=0
+    )
