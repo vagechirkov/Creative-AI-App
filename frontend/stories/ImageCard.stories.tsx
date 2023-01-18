b7c9d21a6d29120e6e6f40127774aff222b58f48
vagechirkov <vagechirkov@gmail.com>
2023-01-18 20:58:03 +0100

    add more stories

--- frontend/stories/ImageCard.stories.tsx
+++ frontend/stories/ImageCard.stories.tsx
@@ -14,10 +14,37 @@ const Template: ComponentStory<typeof ImageCard> = (args) => {
     )
 };
 
-export const DefaultImageCard = Template.bind({});
+export const Active = Template.bind({});
 
-DefaultImageCard.args = {
-    imageUrl: "https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/2d9b748e-754a-47a9-ad42-5496fd9a9941",
+Active.args = {
+    imageUrl: "https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/2117aa24-a983-42e9-a910-912e6ac5ce1f",
+    altText: "Image 1",
+    reactions: [
+        { emoji: "🤔", count: 10 },
+        { emoji: "😍", count: 2 },
+        { emoji: "🤪", count: 10 }],
+    onReaction: (emoji: string) => console.log(emoji),
+    interactive: true,
+}
+
+export const Hoover = Template.bind({});
+
+Hoover.args = {
+    imageUrl: "https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/250eac75-7fb3-4484-944e-4b11dd9535c4",
+    altText: "Image 1",
+    reactions: [
+        { emoji: "🤔", count: 10 },
+        { emoji: "😍", count: 2 },
+        { emoji: "🤪", count: 10 }],
+    onReaction: (emoji: string) => console.log(emoji),
+    interactive: true,
+}
+
+
+export const Tutorial = Template.bind({});
+
+Tutorial.args = {
+    imageUrl: "https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/4e49ae42-4659-4f05-9ae7-ba5dfecda4d6",
     altText: "Image 1",
     reactions: [
         { emoji: "🤔", count: 10 },
