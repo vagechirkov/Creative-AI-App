7de7a98067df2ef127174ba82084a0922e339d71
vagechirkov <vagechirkov@gmail.com>
2023-01-20 21:31:49 +0100

    finish image with actions

--- frontend/components/ImageHover/ImageWithActions.tsx
+++ frontend/components/ImageHover/ImageWithActions.tsx
@@ -12,34 +12,35 @@ interface ImageContainerProps {
 export const ImageWithActions: FC<ImageContainerProps> = ({imageUrl, altText}) => {
 
     return (
-        <div className="grid max-w-sm max-h-sm min-w-max min-h-max grid-cols-12 justify-items-stretch">
-            {/* row 1 */}
-            <div className="col-span-12 text-center">
-                <ActionsLabel> i like it </ActionsLabel>
-            </div>
+        <div className="max-w-sm max-h-sm min-w-max min-h-max">
+            <div className="grid grid-cols-12 gap-2">
+                {/* row 1 */}
+                <div className="col-span-12 text-center">
+                    <ActionsLabel> i like it </ActionsLabel>
+                </div>
 
-            {/* row 2 */}
-            <div className="text-center col-start-1 col-span-1 transform rotate-180 [writing-mode:vertical-lr]">
-                <ActionsLabel> it inspires me </ActionsLabel>
-            </div>
+                {/* row 2 */}
+                <div className="text-center col-start-1 col-span-1 transform rotate-180 [writing-mode:vertical-lr]">
+                    <ActionsLabel> it inspires me </ActionsLabel>
+                </div>
 
-            <div className="col-start-2 col-end-11">
-                <Image
-                    src={imageUrl} alt={altText ? altText : "Image"}
-                    width={300} height={300}
-                    className="border-black border-2 "
-                />
-            </div>
+                <div className="col-start-2 col-end-12">
+                    <Image
+                        src={imageUrl} alt={altText ? altText : "Image"}
+                        width={320} height={320}
+                        className="border-black border-2 "
+                    />
+                </div>
 
-            <div className="text-center col-start-11 col-span-1 transform rotate-360 [writing-mode:vertical-lr]">
-                <ActionsLabel> it surprises me </ActionsLabel>
-            </div>
+                <div className="text-center col-start-12 col-span-1 transform rotate-360 [writing-mode:vertical-lr]">
+                    <ActionsLabel> it surprises me </ActionsLabel>
+                </div>
 
-            {/* row 3 */}
-            <div className="col-span-12 text-center">
-                <ActionsLabel> it terrifies me </ActionsLabel>
+                {/* row 3 */}
+                <div className="col-span-12 text-center">
+                    <ActionsLabel> it terrifies me </ActionsLabel>
+                </div>
             </div>
-
         </div>
     );
 }
