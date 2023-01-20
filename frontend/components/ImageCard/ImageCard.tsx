db2d38a859e1c1b8d654cba5c3306d5466149c30
vagechirkov <vagechirkov@gmail.com>
2023-01-20 12:12:13 +0100

    update image card

--- frontend/components/ImageCard/ImageCard.tsx
+++ frontend/components/ImageCard/ImageCard.tsx
@@ -6,6 +6,7 @@ import {Avatar, Badge, Card, CardContent, CardHeader, CardMedia, IconButton, Sta
 import {red} from "@mui/material/colors";
 import {FeedImageType} from "../FeedImages/FeedImages";
 import {People} from "@mui/icons-material";
+import ReactionsCount from "./ReactionsCount";
 
 
 interface ImageCardProps {
@@ -23,12 +24,12 @@ const ImageCard: FC<ImageCardProps> = (props) => {
 
     return (
         <>
-            <div className="relative max-w-xs max-h-xs text-white text-xs">
+            <div className="relative max-w-sm max-h-sm min-w-max text-white text-xs">
                 <Image
                     src={imageUrl}
                     alt={altText}
-                    width={320}
-                    height={320}
+                    width={384}
+                    height={384}
                     className={"feed-image"}
                 />
                 <div
@@ -36,7 +37,7 @@ const ImageCard: FC<ImageCardProps> = (props) => {
                     absolute h-2/3
                     flex flex-col justify-end
                     inset-x-0
-                    bottom-0 px-4 py-2 max-w-xs
+                    bottom-0 px-4 py-2 max-w-sm
                     bg-gradient-to-b from-transparent to-black"
                 >
                     <p className="text-bold">
@@ -46,13 +47,10 @@ const ImageCard: FC<ImageCardProps> = (props) => {
                         {altText}
                     </p>
                     <div className="flex grid justify-between grid-cols-2 gap-1">
-                        <span>
-                            <span className="text-bold"> LOVE IT</span>
-
-                        </span>
-                        <span>CURIOUS</span>
-                        <span>INDIFFERENT</span>
-                        <span>MAKES ME SAD</span>
+                        <ReactionsCount text={"love it"} count={100}/>
+                        <ReactionsCount text={"curious"} count={100}/>
+                        <ReactionsCount text={"indifferent"} count={100}/>
+                        <ReactionsCount text={"makes me sad"} count={100}/>
                     </div>
                 </div>
             </div>
