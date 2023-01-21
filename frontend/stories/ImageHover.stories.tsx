5c7b1ae0f00cbae69040d947ff46be2aa524f03e
vagechirkov <vagechirkov@gmail.com>
2023-01-21 11:56:09 +0100

    publish stories again

--- frontend/stories/ImageHover.stories.tsx
+++ frontend/stories/ImageHover.stories.tsx
@@ -22,15 +22,3 @@ Default.args = {
     altText: "Smiling monster made of food",
     onReactions: (direction: string) => console.log(direction),
 }
-
-export const WithBackground = Template.bind({});
-
-WithBackground.args = {
-    imageUrl: "https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/250eac75-7fb3-4484-944e-4b11dd9535c4",
-    altText: "Smiling monster made of food",
-    onReactions: (direction: string) => console.log(direction),
-    initialBackground: {
-        backgroundText: background.Down,
-        opacity: 1
-    }
-}
