4e3897d507372ff607cb4bbae9e9a33d412aadd6
vagechirkov <vagechirkov@gmail.com>
2023-01-21 11:21:29 +0100

    estimate direction and opacity

--- frontend/components/ImageHover/ImageHover.tsx
+++ frontend/components/ImageHover/ImageHover.tsx
@@ -2,7 +2,7 @@
 
 import {FC, useState} from "react";
 import {ImageWithActions} from "./ImageWithActions";
-import {SwipeEventData, useSwipeable} from "react-swipeable";
+import Draggable from 'react-draggable';
 import {background} from "./background";
 
 type backgroundState = {
@@ -19,48 +19,41 @@ interface ImageHoverProps {
 
 const swipeConfig = {
     trackMouse: true,
-    preventScrollOnSwipe: true,
+    preventScrollOnSwipe: false,
 }
 
 const ImageHover: FC<ImageHoverProps> = (props) => {
     const {imageUrl, altText, onReactions, initialBackground = {backgroundText: '', opacity: 0}} = props;
     const [bgState, setBgState] = useState<backgroundState>(initialBackground);
 
-    const handlers = useSwipeable({
-            onSwiped: (eventData) => handleSwipe(eventData),
-            onSwiping: (eventData) => changeBackground(eventData),
-            onSwipeStart: (eventData) => handleSwipeStart(eventData),
-            ...swipeConfig
-        },
-    );
+    const handleDrag = (event: any, data: any) => {
+        const direction = handleResponseDirection({x: data.x, y: data.y});
+        const magnitude = handleResponseMagnitude({x: data.x, y: data.y, maxMagnitude: 200});
 
-    const handleSwipeStart = (eventData: SwipeEventData) => {
-        // setShowActions(true);
-        // console.log("Swipe start");
-    }
-
-    const changeBackground = (eventData: SwipeEventData) => {
-        console.log(eventData.dir);
-        switch (eventData.dir) {
+        switch (direction) {
             case 'Left':
-                setBgState({backgroundText: background.Left, opacity: 1});
+                setBgState({backgroundText: background.Left, opacity: magnitude});
                 break;
             case 'Right':
-                setBgState({backgroundText: background.Right, opacity: 1});
+                setBgState({backgroundText: background.Right, opacity: magnitude});
                 break;
             case 'Up':
-                setBgState({backgroundText: background.Up, opacity: 1});
+                setBgState({backgroundText: background.Up, opacity: magnitude});
                 break;
             case 'Down':
-                setBgState({backgroundText: background.Down, opacity: 1});
+                setBgState({backgroundText: background.Down, opacity: magnitude});
                 break;
             default:
                 setBgState(initialBackground);
         }
     }
 
-    const handleSwipe = (eventData: SwipeEventData) => {
-        onReactions(eventData.dir);
+    const handleDragStart = (event: any, data: any) => {
+
+    }
+
+    const handleDragStop = (event: any, data: any) => {
+        console.log(data);
     }
 
 
@@ -75,13 +68,47 @@ const ImageHover: FC<ImageHoverProps> = (props) => {
 
             {/* image with actions */}
             <div className="fixed h-full w-full flex items-center justify-center">
-                <div {...handlers} className="z-10 cursor-pointer">
-                    <ImageWithActions imageUrl={imageUrl} altText={altText}/>
-                </div>
+                <Draggable
+                    defaultPosition={{x: 0, y: 0}}
+                    position={{x: 0, y: 0}}
+                    onStop={handleDragStop}
+                    onDrag={handleDrag}
+                >
+                    <div className="z-10 cursor-pointer">
+
+                        <ImageWithActions imageUrl={imageUrl} altText={altText}/>
+                    </div>
+                </Draggable>
             </div>
         </div>
 
     );
 };
+
+
+const handleResponseDirection = ({x, y}: { x: number, y: number }) => {
+    // range (-PI, PI]
+    let theta = Math.atan2(x, y);
+
+    // rads to degrees, range (-180, 180]
+    theta *= 180 / Math.PI;
+
+    // range [0, 360); NOTE: 0 is at the bottom
+    if (theta < 0) theta = 360 + theta;
+
+    // estimate direction
+    if (theta >= 45 && theta < 135) return 'Right';
+    if (theta >= 135 && theta < 225) return 'Up';
+    if (theta >= 225 && theta < 315) return 'Left';
+    if (theta >= 315 || theta < 45) return 'Down';
+}
+
+const handleResponseMagnitude = ({x, y, maxMagnitude = 200}: { x: number, y: number, maxMagnitude: number }) => {
+    let magnitude = Math.sqrt(x * x + y * y);
+
+    // normalize magnitude to [0, 1]
+    if (magnitude > maxMagnitude) return 100;
+    return (magnitude / maxMagnitude) * 100;
+}
 
 export default ImageHover;
