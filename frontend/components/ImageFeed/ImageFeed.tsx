9838b033e531eb3684d870f64cd3276482ad7d61
vagechirkov <vagechirkov@gmail.com>
2023-01-23 13:13:57 +0100

    add black square to hide previous image

--- frontend/components/ImageFeed/ImageFeed.tsx
+++ frontend/components/ImageFeed/ImageFeed.tsx
@@ -10,7 +10,7 @@ interface ImageFeedProps {
     isCurrentImageUpdated?: boolean;
 }
 
-const ImageFeed: FC<ImageFeedProps> = ({isCurrentImageUpdated=false}) => {
+const ImageFeed: FC<ImageFeedProps> = ({isCurrentImageUpdated = false}) => {
     const {feedState, feedDispatch} = useFeedContext();
     const feedEndRef = useRef<null | HTMLDivElement>(null);
 
@@ -46,27 +46,26 @@ const ImageFeed: FC<ImageFeedProps> = ({isCurrentImageUpdated=false}) => {
                 {/* history (NOTE: the last one is the duplicate of the current image)*/}
                 {feedState?.feedHistory && !feedState.dragState.isDragging &&
                     feedState.feedHistory.map((imageCard, index) => (
-                    <div key={`card-${index}`} className="snap-center flex justify-center bg-transparent">
-                        <ImageWithReactions
-                            imageUrl={imageCard.url}
-                            altText={imageCard.alt_text}
-                            reactions={imageCard.reactions}
-                            activeUsers={imageCard.active_users}
-                        />
-                    </div>
-                ))}
+                        <div key={`card-${index}`} className="snap-center flex justify-center bg-transparent">
+                            <ImageWithReactions
+                                imageUrl={imageCard.url}
+                                altText={imageCard.alt_text}
+                                reactions={imageCard.reactions}
+                                activeUsers={imageCard.active_users}
+                            />
+                        </div>
+                    ))}
 
                 {/* current image */}
                 <div key={`card-current`} className="snap-center pb-[160px] flex justify-center mt-auto">
                     <span
-                        className={`${isCurrentImageUpdated ? 
-                            "transform translate-y-full opacity-0" : 
-                            "transition-all duration-200 opacity-100 transform translate-y-0"}`}>
+                        className={`transition-all duration-200 ${isCurrentImageUpdated ? "opacity-0" : "opacity-100"}`}
+                    >
                     {feedState?.currentImage &&
                         <ImageDraggable onReactions={handleDrag}>
                             <div className="cursor-move w-fit">
                                 <ImageWithReactions
-                                    imageUrl={feedState.currentImage.url}
+                                    imageUrl={isCurrentImageUpdated ? '/images/black_square.png' : feedState.currentImage.url}
                                     altText={feedState.currentImage.alt_text}
                                     reactions={feedState.currentImage.reactions}
                                     activeUsers={feedState.currentImage.active_users}
