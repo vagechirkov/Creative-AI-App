db8baacc8d0584d9ce56c452b5bdc4638ce1e1c2
vagechirkov <vagechirkov@gmail.com>
2023-01-23 14:35:04 +0100

    allow to submit prompt

--- frontend/components/FeedPage/FeedFooter.tsx
+++ frontend/components/FeedPage/FeedFooter.tsx
@@ -1,8 +1,15 @@
 import useFeedContext from "../FeedContext";
+import {useState} from "react";
 
 
 const FeedFooter = () => {
+    const [newPrompt, setNewPrompt] = useState<string>('');
     const {feedState} = useFeedContext();
+
+    const onPromptSubmit = () => {
+        console.log(newPrompt);
+        setNewPrompt('');
+    }
 
     return (
         <div
@@ -14,12 +21,18 @@ const FeedFooter = () => {
                     <span className="font-plex font-normal">
                     <input id="new_prompts"
                            className="text-black bg-white font-normal w-64 h-10 pl-3 focus:outline-none flex items-center border-gray-300 rounded"
-                           placeholder="Enter your prompt here"/>
+                           placeholder="Enter your prompt here"
+                           value={newPrompt}
+                           onChange={(e) => setNewPrompt(e.target.value)}
+                    />
                     </span>
                 </div>
 
-                <div className="text-center col-span-1">
-                    <button className="w-fit h-fit px-2 border border-black bg-black hover:bg-white text-white hover:text-black">
+                <div className="text-center col-span-1 pl-2">
+                    <button
+                        className="w-fit h-fit border border-black bg-black hover:bg-white text-white hover:text-black"
+                        onClick={onPromptSubmit}
+                    >
                         <div className="font-jura ">
                             Propose
                         </div>
